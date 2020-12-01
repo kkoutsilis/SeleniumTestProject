7c73601f76b5c7d326752964ddfbbe2707d7a4e4
kkoutsilis <kkoutsilis@gmail.com>
2020-12-01 14:28:56 +0200

    second test done

---
+++ selenium_test2.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+PATH = ("C:\Program Files (x86)\chromedriver.exe")
+driver = webdriver.Chrome(PATH)
+
+driver.get("http://ict.ihu.gr/")
+
+link = driver.find_element_by_link_text("Νέο πρόγραμμα σπουδών 5ετούς διάρκειας")
+link.click()
+
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT,"Ανακοινώσεις"))
+    )
+    element.click()
+
+    element2 = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT,"ΠΑΙΔΑΓΩΓΙΚΑ"))
+    )
+    element2.click()
+
+    driver.back()
+    driver.back()
+    driver.back()
+except:
+    driver.quit()
+
+time.sleep(2)
+driver.quit()
+
